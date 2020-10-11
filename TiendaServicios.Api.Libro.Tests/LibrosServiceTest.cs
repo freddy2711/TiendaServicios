b993d979553f0a7616bced1a43baefdfd240a7e7
freddy2711 <freddy2711@gmail.com>
2020-10-11 18:43:50 -0500

    cambios CI

--- TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -21,7 +21,7 @@ namespace TiendaServicios.Api.Libro.Tests
 
         private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
         {
-            //Metodo para llenar con data de genfu 333
+            //Metodo para llenar con data de genfu 333   fgfg
 
             A.Configure<LibreriaMaterial>()
                 .Fill(x => x.Titulo).AsArticleTitle()
